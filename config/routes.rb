be2f804a82b8202053c768c1966c2b2f937e0c94
gitbike <git_bike_840@yahoo.co.jp>
2019-01-10 19:48:02 +0900

    MembersControllerを生成。:membersリソースをroutes.rbに追加

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   # get 'lesson/:action(/:name)', to: 'lesson'
   # get 'lesson/step1/:name', to: 'lesson#step1'
   get 'lesson/:action(/:name)' => 'lesson'
+
+  resources :members
 end
